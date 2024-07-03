6e6744228e9438abf22e46c114a10e14efcd8ee8
Farizptr <18221015@std.stei.itb.ac.id>
2024-07-03 16:29:20 +0700

    update order

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -140,7 +140,11 @@ const OrderScreen = () => {
     ];
     const monthIndex = date.getMonth();
     const year = date.getFullYear();
-    return `${day} ${monthNames[monthIndex]} ${year}`;
+
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+    return `${day} ${monthNames[monthIndex]} ${year} ${hours}:${minutes}:${seconds}`;
   }
 
   return (
