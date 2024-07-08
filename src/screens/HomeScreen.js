cf63eb476a6d14973f04d38a5b0428b283ec23ee
Daffa Saskara <daffasasds@gmail.com>
2024-07-08 08:20:53 +0700

    feat: UI

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ImageBackground, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useRole } from "../context/RoleContext";
 
@@ -7,22 +7,86 @@ const HomeScreen = () => {
   const navigation = useNavigation();
   const { role } = useRole();
 
-  // Your code here
 
   return (
-    <View>
-      {/* Your JSX here */}
-      <Button
-        title="Go to Order"
-        onPress={() => navigation.navigate("Order")}
-      />
-      <Button
-        title="Go to Admin Screen"
-        onPress={() => navigation.navigate("ProcurementAdmin")}
-      />
-      
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('C:/Users/daffa/Downloads/TUGAS KULIAH DAFFA SASKARA/elog/eReservation/src/assets/images/KSlenskep.png')}
+        style={styles.backgroundImage}
+        imageStyle={{ opacity: 0.9 }}
+      >
+        <Image source={require('C:/Users/daffa/Downloads/TUGAS KULIAH DAFFA SASKARA/elog/eReservation/src/assets/images/welcom.png')} style={styles.welcome} />
+        <Text style={styles.welcomeText}>{ role }</Text>
+      </ImageBackground>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Order")}>
+          <Image source={require('C:/Users/daffa/Downloads/TUGAS KULIAH DAFFA SASKARA/elog/eReservation/src/assets/images/orderlogofix.png')} style={styles.buttonIcon} />
+          <Text style={styles.buttonText}>Orders</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("ProcurementAdmin")}>
+          <Image source={require('C:/Users/daffa/Downloads/TUGAS KULIAH DAFFA SASKARA/elog/eReservation/src/assets/images/adminlogofix.png')} style={styles.buttonIcon} />
+          <Text style={styles.buttonText}>Admin</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: 200,
+    justifyContent: "center",
+    alignItems: "left",
+  },
+  welcome: {
+    width: "80%",
+    height: "80%",
+    marginLeft: 20,
+    marginBottom: 80,
+    resizeMode: "contain",
+  },
+  welcomeText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    justifyContent: "left",
+    color: "#FFFFFF",
+    //backgroundColor: "rgba(255, 255, 255, 0.7)",
+    paddingVertical: 0,
+    marginTop: -140,
+    marginLeft: 20,
+    paddingHorizontal: 5,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    flex: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+  
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    padding: 15,
+    borderRadius: 8,
+    marginVertical: 30,
+    width: "70%",
+  },
+  buttonIcon: {
+    width: 160,
+    height: 160,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+});
 
 export default HomeScreen;
