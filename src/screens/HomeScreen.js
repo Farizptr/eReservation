755eabd1249c83f8dcb5db60bc752eb3835d65fa
Farizptr <18221015@std.stei.itb.ac.id>
2024-07-08 08:45:22 +0700

    fix auth

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,34 +1,59 @@
 import React from "react";
-import { View, Text, Button, ImageBackground, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useRole } from "../context/RoleContext";
+import { ScrollView } from "react-native-gesture-handler";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { role } = useRole();
 
-
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require('../assets/images/KSlenskep.png')}
-        style={styles.backgroundImage}
-        imageStyle={{ opacity: 0.9 }}
-      >
-        <Image source={require('../assets/images/welcom.png')} style={styles.welcome} />
-        <Text style={styles.welcomeText}>{ role }</Text>
-      </ImageBackground>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Order")}>
-          <Image source={require('../assets/images/orderlogofix.png')} style={styles.buttonIcon} />
-          <Text style={styles.buttonText}>Orders</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("ProcurementAdmin")}>
-          <Image source={require('../assets/images/adminlogofix.png')} style={styles.buttonIcon} />
-          <Text style={styles.buttonText}>Admin</Text>
-        </TouchableOpacity>
+    <ScrollView>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../assets/images/KSlenskep.png")}
+          style={styles.backgroundImage}
+          imageStyle={{ opacity: 0.9 }}
+        >
+          <Image
+            source={require("../assets/images/welcom.png")}
+            style={styles.welcome}
+          />
+          <Text style={styles.welcomeText}>{role}</Text>
+        </ImageBackground>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("Order")}
+          >
+            <Image
+              source={require("../assets/images/orderlogofix.png")}
+              style={styles.buttonIcon}
+            />
+            <Text style={styles.buttonText}>Orders</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("Admin")}
+          >
+            <Image
+              source={require("../assets/images/adminlogofix.png")}
+              style={styles.buttonIcon}
+            />
+            <Text style={styles.buttonText}>Admin</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -68,7 +93,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   button: {
-  
     alignItems: "center",
     backgroundColor: "rgba(255, 255, 255, 0.7)",
     padding: 15,
